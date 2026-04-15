9b0834fd046ed8a5d69f82e962b6af2d869637cb
jonathancesar <jonathancesar1995@gmail.com>
2026-04-14 21:33:56 -0300

    Implementando diretiva de validação

--- src/app/shared/directives/input-validation-directive.ts
+++ src/app/shared/directives/input-validation-directive.ts
@@ -1,17 +1,43 @@
-import { Directive, HostListener, Input } from '@angular/core';
+import { DestroyRef, Directive, HostListener, inject, Input } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { NgControl } from '@angular/forms';
 
 @Directive({
   selector: '[appInputValidation]',
 })
 export class InputValidationDirective {
+  private readonly _ngControl = inject(NgControl, { self: true });
+  private readonly _destroyRef = inject(DestroyRef);
+
   @Input() errorMessage = 'Campo Inválido';
 
   ngOnInit() {
     console.log('OnInit errorMessage', this.errorMessage);
+
+    this._ngControl.statusChanges?.pipe(takeUntilDestroyed(this._destroyRef)).subscribe(() => {
+      console.log('Status mudou!');
+
+      this.updateStatus();
+    });
   }
 
   @HostListener('blur')
   onBlur() {
     console.log('Ocorreu um blur');
+    this.updateStatus();
+  }
+
+  updateStatus() {
+    const control = this._ngControl.control;
+
+    if (!control) return;
+
+    const isInvalid = control.invalid && control.touched;
+
+    if (isInvalid) {
+      console.log('Está inválido:');
+    } else {
+      console.log('Está válido:');
+    }
   }
 }
